2dce6470c802b4921a44343380f2583f28885d58
SivaDev <42565813+siva1b3@users.noreply.github.com>
2024-02-29 14:54:48 +0530

    002 start

--- myApp01/src/app/app0201/app0201.component.ts
+++ myApp01/src/app/app0201/app0201.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app0201',
+  selector: 'App0201',
   templateUrl: './app0201.component.html',
-  styleUrls: ['./app0201.component.css']
+  styleUrls: ['./app0201.component.css'],
 })
 export class App0201Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- myApp01/src/app/app0202/app0202.component.ts
+++ myApp01/src/app/app0202/app0202.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app0202',
+  selector: 'App0202',
   templateUrl: './app0202.component.html',
-  styleUrls: ['./app0202.component.css']
+  styleUrls: ['./app0202.component.css'],
 })
 export class App0202Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
