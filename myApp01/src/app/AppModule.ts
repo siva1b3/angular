ae3f58f7c954fb02e154da678db3986a4cf92053
SivaDev <42565813+siva1b3@users.noreply.github.com>
2024-03-07 12:59:28 +0530

    aa

---
+++ myApp01/src/app/AppModule.ts
@@ -0,0 +1,15 @@
+﻿import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { ClassSelector05Component } from "./002The Basics/class-selector05/class-selector05.component";
+import {AppModule} from "./app.module";
+
+
+@NgModule({
+    declarations: [AppComponent, ClassSelector05Component],
+  imports: [BrowserModule, AppModule, AppModule, AppModule],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {
+}
