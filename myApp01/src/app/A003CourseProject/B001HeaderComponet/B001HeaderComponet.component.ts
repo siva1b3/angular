3f3cee774fa1ede8cd141cfb107bbe3fe712bae0
SivaDev <42565813+siva1b3@users.noreply.github.com>
2024-04-14 21:40:44 +0530

    003 start again

--- myApp01/src/app/A003CourseProject/B001HeaderComponet/B001HeaderComponet.component.ts
+++ myApp01/src/app/A003CourseProject/B001HeaderComponet/B001HeaderComponet.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "B001HeaderComponet",
   templateUrl: "./B001HeaderComponet.component.html",
-  styleUrls: ["./B001HeaderComponet.component.css"],
+  styleUrls: ["./B001HeaderComponet.component.scss"],
 })
 export class B001HeaderComponetComponent implements OnInit {
   constructor() {}
